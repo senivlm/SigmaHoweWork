8b9813dafae7d6d639172491bbc7a86908e72da8
senivlm <91743906+senivlm@users.noreply.github.com>
2022-05-30 22:42:00 +0300

    Update Program.cs

--- Sigma1/Program.cs
+++ Sigma1/Program.cs
@@ -5,7 +5,7 @@ namespace Sigma1
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {// Де пехоплюються винятки, які описані в класах проекту.
             Storage storage = new Storage();
             storage.Dialogue();
         }
