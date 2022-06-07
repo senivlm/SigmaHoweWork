f1c0e6994f431311ec7e10400755c0a62037850f
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-07 13:36:20 +0300

    Update Program.cs

--- Sigma_05.19/Program.cs
+++ Sigma_05.19/Program.cs
@@ -5,7 +5,7 @@ namespace Sigma_05._19
     class Program
     {
         static void Main(string[] args)
-        {
+        {// якщо статичний, то вже краще в іншому класі-сервісі
             Vector.FileSplitMergeSort("file1.txt", "file2.txt");
         }
     }
